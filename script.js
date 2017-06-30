287a28ccaa3e64589db27550f352a8be64705622
Reid Trierweiler <RT.Trierweiler@gmail.com>
2017-06-30 09:10:27 -0400

    NaN fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var fahrTemp = parseInt(prompt("Enter Fahrenheit temperature. (Numbers only)"));
-var celsTemp = parseInt(prompt("Enter Celsius temperature. (Numbers only)"));
+var fahrTemp = parseInt(prompt("Enter Fahrenheit temperature. (Numbers only)")) || 0;
+var celsTemp = parseInt(prompt("Enter Celsius temperature. (Numbers only)")) || 0;
 var fToC =  ((fahrTemp - 32)/1.8);
 var cToF = (celsTemp*1.8)+32;
 console.log(fahrTemp + "°F Entered = " + fToC + "°C");
